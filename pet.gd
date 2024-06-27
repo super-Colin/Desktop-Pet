bee050aae7a14c1e076a31e5b5dc70b12326edb7
superColin <supercolin.dev@gmail.com>
2024-06-27 16:23:00 -0400

    changes sprite image while being dragged

--- pet.gd
+++ pet.gd
@@ -2,7 +2,12 @@ extends Control
 
 
 var mouseStart: Vector2i
-var moving = false
+var beingDragged = false
+
+
+var sprite_standing = load("res://pet_assets/flyMan_still_stand.png") 
+var sprite_flying = load("res://pet_assets/flyMan_fly.png") 
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,20 +21,30 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if moving:
+	if beingDragged: # set by onDrag
 		var mouseDelta = Vector2i(get_viewport().get_mouse_position())
 		get_window().position += mouseDelta - mouseStart
-
+	handleDragAnimation()
 
 
 
 
 func onDrag(event)->void:
-	#print(event)
 	if event is InputEventMouseButton and event.button_index == 1:
-		#mouseStart = 
-		if ! moving:
+		if ! beingDragged:
 			mouseStart = get_viewport().get_mouse_position()
-		moving = event.pressed
+		beingDragged = event.pressed
+
+func handleDragAnimation():
+	# change sprite on drag
+	if beingDragged:
+		$Icon.texture = sprite_flying
+	else:
+		$Icon.texture = sprite_standing
+		
+	#print($Icon.texture)
+	pass
+
+
 
 
