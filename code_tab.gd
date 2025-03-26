d02b06e410f096fde3f3d093abe378fc99b18eed
superColin <supercolin.dev@gmail.com>
2025-03-26 18:14:50 -0400

    can CRUD snippets

--- code_tab.gd
+++ code_tab.gd
@@ -1,17 +1,55 @@
 extends VBoxContainer
 
+var snippets:Dictionary = {}
+var snippetRowScene = preload("res://snippet_row.tscn")
 
+const SAVE_SECTION = "code_tab"
 
 
 func _ready() -> void:
-	%NewSnippetButton.pressed.connect(openNewSnippetPad)
-
-
-func openNewSnippetPad():
-	pass
-	# Open editable text window
+	snippets = Saver.loadGameSection(SAVE_SECTION, "snippets", snippets)
 	
+	%NewSnippetButton.pressed.connect(openSnippetPad)
+	%SnippetCancelButton.pressed.connect(closeSnippetPad)
+	%SnippetSaveButton.pressed.connect(saveNewSnippet)
+	
+	refreshSnippetsList()
+
+func saveNewSnippet():
+	print("code - editor input is: ", %CodeEditNode.text)
+	snippets[%SnippetName.text] = %CodeEditNode.text
+	saveSnippets()
+	refreshSnippetsList()
+
+func saveSnippets():
+	Saver.saveGameSection(SAVE_SECTION, "snippets", snippets)
+
+func refreshSnippetsList():
+	for c in %SnippetList.get_children():
+		c.queue_free()
+	for key in snippets.keys():
+		var newRow = snippetRowScene.instantiate()
+		newRow.setUp(key, snippets[key])
+		newRow.editRequested.connect(editExistingSnippet)
+		newRow.deleteRequested.connect(deleteExistingSnippet)
+		%SnippetList.add_child(newRow)
+
+func deleteExistingSnippet(snippetName:String):
+	snippets.erase(snippetName)
+	saveSnippets()
+	refreshSnippetsList()
+
+func editExistingSnippet(snippetName:String):
+	openSnippetPad()
+	%CodeEditNode.text = snippets[snippetName]
+	%SnippetName.text = snippetName
 
 
+func openSnippetPad():
+	$SnippetEditor.visible = true
+
+func closeSnippetPad():
+	$SnippetEditor.visible = false
+	$SnippetEditor.text = ""
 
 #
---
+++ save_manager.gd
@@ -0,0 +1,72 @@
+extends Node
+
+
+@onready var saveGameConfigNode:ConfigFile = ConfigFile.new()
+
+
+const SAVE_DIRECTORY_PATH = "user://"
+const SAVE_FILE_TITLE = "config"
+const SAVE_FILE_PATH = SAVE_DIRECTORY_PATH + SAVE_FILE_TITLE + ".cfg"
+
+
+signal saveLoaded
+signal saved
+
+
+
+func _ready() -> void:
+	initialLoad()
+
+
+func initialLoad():
+	var err = saveGameConfigNode.load(SAVE_FILE_PATH)
+	if err != OK:
+		print("saves - error loading save file: ", err, ", -- making new file --")
+		await initNewSaveFile()
+	else: print("saves - loaded save file successfully")
+	saveLoaded.emit()
+	return true
+
+
+func saveGameSection(sectionName, sectionKey, sectionData):
+	if saveGameConfigNode == null:print("saves - saveGameSection didn't have config var")
+	saveGameConfigNode.set_value(sectionName, sectionKey, sectionData)
+	saveGameConfigNode.save(SAVE_FILE_PATH)
+	saved.emit()
+
+
+func loadGameSection(sectionName, sectionKey, fallback):
+	return saveGameConfigNode.get_value(sectionName, sectionKey, fallback)
+
+
+func initNewSaveFile():
+	print("saves - initing new save")
+	if ResourceLoader.exists(SAVE_FILE_PATH):
+		var result = DirAccess.remove_absolute(SAVE_FILE_PATH)
+		if result != 0:
+			print("save - couldn't delete previous save file, code: ", result)
+		else: print("save - deleted previous save file, code: ", result)
+	saveGameConfigNode = ConfigFile.new()
+	reset_saveStates_all()
+
+
+func save_to_file(fileName:String, fileContent:String, fileExtension:String=".txt", directory:String=""):
+	var file = FileAccess.open("user://" + fileName + fileExtension, FileAccess.WRITE)
+	file.store_string(fileContent)
+
+func load_from_file(fileName:String, fileContent:String, fileExtension:String=".txt", directory:String="", isString:bool=true):
+	var file = FileAccess.open("user://" + fileName + fileExtension, FileAccess.READ)
+	var content = file.get_as_text()
+	return content
+
+
+
+
+func saveGame_all():
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	for node in save_nodes:
+		get_tree().call_group("save", "_save")
+
+func reset_saveStates_all():
+	get_tree().call_group("save", "_resetSaveState")
+	saveGame_all()
